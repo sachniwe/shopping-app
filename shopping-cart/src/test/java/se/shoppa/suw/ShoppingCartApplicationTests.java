eb186e8d0131aa0697deb0ba6c1e2cac3f959c22
sachniwe <44708156+sachniwe@users.noreply.github.com>
2020-12-13 21:30:52 +0200

    initial commit

---
+++ shopping-cart/src/test/java/se/shoppa/suw/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package se.shoppa.suw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
