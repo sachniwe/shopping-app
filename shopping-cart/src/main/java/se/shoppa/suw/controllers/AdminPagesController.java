69c93cdf6047d0da83c637df52ae6b65585f4820
sachniwe <44708156+sachniwe@users.noreply.github.com>
2020-12-16 00:06:35 +0200

    added edit and delete

--- shopping-cart/src/main/java/se/shoppa/suw/controllers/AdminPagesController.java
+++ shopping-cart/src/main/java/se/shoppa/suw/controllers/AdminPagesController.java
@@ -1,5 +1,6 @@
 package se.shoppa.suw.controllers;
 
+
 import java.util.List;
 
 import javax.validation.Valid;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -20,60 +22,114 @@ import se.shoppa.suw.models.data.Page;
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
-	
+
 	@Autowired
 	private PageRepository pageRepo;
-	
+
 //	public AdminPagesController(PageRepository pageRepo) {
 //		this.pageRepo = pageRepo;
 //		
 //	}
-	
+
 	@GetMapping
 	public String index(Model model) {
 		List<Page> pages = pageRepo.findAll();
-		//to pass all pages to admin/pages/index
+		// to pass all pages to admin/pages/index
 		model.addAttribute("pages", pages);
-		
+
 		return "admin/pages/index";
 	}
-	
-	
+
 	@GetMapping("/add")
-    public String add(@ModelAttribute Page page, Model model) {
-        
-        model.addAttribute("page", new Page());
+	public String add(@ModelAttribute Page page, Model model) {
 
-        return "admin/pages/add";
-    }
-	
-	 @PostMapping("/add")
-	    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+		model.addAttribute("page", new Page());
 
-	        if (bindingResult.hasErrors()) {
-	            return "admin/pages/add";
-	        }
+		return "admin/pages/add";
+	}
 
-	        redirectAttributes.addFlashAttribute("message", "Page added");
-	        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+	@PostMapping("/add")
+	public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+			Model model) {
 
-	        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+		if (bindingResult.hasErrors()) {
+			return "admin/pages/add";
+		}
 
-	        Page slugExists = pageRepo.findBySlug(slug);
+		redirectAttributes.addFlashAttribute("message", "Page added");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 
-	        if ( slugExists != null ) {
-	            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
-	            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
-	            redirectAttributes.addFlashAttribute("page", page);
+		String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-")
+				: page.getSlug().toLowerCase().replace(" ", "-");
 
-	        } else {
-	            page.setSlug(slug);
-	            page.setSorting(100);
+		Page slugExists = pageRepo.findBySlug(slug);
 
-	            pageRepo.save(page);
-	        }
+		if (slugExists != null) {
+			redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectAttributes.addFlashAttribute("page", page);
 
-	        return "redirect:/admin/pages/add";
-	    }
+		} else {
+			page.setSlug(slug);
+			page.setSorting(100);
 
+			pageRepo.save(page);
+		}
+
+		return "redirect:/admin/pages/add";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model model) {
+		Page page = pageRepo.getOne(id);
+		model.addAttribute("page", page);
+		return "/admin/pages/edit";
+
+	}
+
+	@PostMapping("/edit")
+	public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+			Model model) {
+
+		Page pageCurrent = pageRepo.getOne(page.getId());
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("pageTitle",pageCurrent.getTitle());
+			return "admin/pages/edit";
+		}
+
+		redirectAttributes.addFlashAttribute("message", "Page edited");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+		String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-")
+				: page.getSlug().toLowerCase().replace(" ", "-");
+
+		Page slugExists = pageRepo.findBySlug(page.getId(), page.getSlug());
+
+		if (slugExists != null) {
+			redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectAttributes.addFlashAttribute("page", page);
+
+		} else {
+			page.setSlug(slug);
+			page.setSorting(100);
+
+			pageRepo.save(page);
+		}
+
+		return "redirect:/admin/pages/edit/"+page.getId();
+	}
+
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
+		
+		pageRepo.deleteById(id);
+
+		redirectAttributes.addFlashAttribute("message", "Page deleted");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		return "redirect:/admin/pages/";
+
+	}
 }
--- shopping-cart/src/main/java/se/shoppa/suw/models/PageRepository.java
+++ shopping-cart/src/main/java/se/shoppa/suw/models/PageRepository.java
@@ -3,11 +3,16 @@ package se.shoppa.suw.models;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import se.shoppa.suw.models.data.Page;
 
 public interface PageRepository extends JpaRepository<Page, Integer>{
 	//check slug exists
 	Page findBySlug(String slug);
+	
+	
+	@Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+	Page findBySlug(int id, String slug);
 
 }
